2805c13499009db85acb5e9919748f76b9241a11
daria-dc <daria.doncevic@gmail.com>
2022-10-25 14:03:28 +0200

    extract specific term

--- 03_analysis/03_LGMD/src/03_perform_systematic_knockout_and_extract_LGMD_activities.py
+++ 03_analysis/03_LGMD/src/03_perform_systematic_knockout_and_extract_LGMD_activities.py
@@ -32,38 +32,46 @@ onto_genes = hpo.extract_genes(top_thresh=1000,
                                bottom_thresh=10)
 
 
-
-# perform systematic knockout
-ko_act = []
-
-for g in onto_genes:
-
-    act = hpo_model.perturbation(ontobj=hpo,
-                                 dataset='recount3_GTEx',
-                                 genes=[g],
-                                 values=[0])
-    ko_act.append(act)
-
-ko_act = np.vstack(ko_act).T 
-
-
 # load GTEx annotation
 sample_annot = pd.read_csv(project_dir + '/datasets/recount3_GTEx/recount3_GTEx_annot.csv')
 muscle_idx = sample_annot[sample_annot.tissue == 'Muscle'].index.to_numpy()
 
-
-# find genes that were not 0 in all muscle samples in dataset
+# extract dataset
 data = hpo.extract_data('recount3_GTEx',
                         top_thresh=1000,
                         bottom_thresh=10)
-muscle_data = data[muscle_idx,:]
-good_gene_ids = np.where(np.sum(muscle_data, axis=0) != 0)[0]
 
-# subset
-ko_act = pd.DataFrame(ko_act[muscle_idx, good_gene_ids])
-ko_act.columns = np.array(onto_genes)[good_gene_ids]
+# subset to muscle samples
+data = data[muscle_idx,:]
+
+# get genes whose expression is not 0 for all samples
+good_gene_ids = np.where(np.sum(muscle_data, axis=0) != 0)[0]
+good_genes = np.array(onto_genes)[good_gene_ids]
+
+# add subsetted dataset to Ontoobj
+hpo.add_dataset(dataset=data,
+                description='recount3_GTEx_muscle',
+                top_thresh=1000,
+                bottom_thresh=10)
+
+# perform systematic knockout in muscle
+ko_act = np.zeros((data.shape[0],len(good_genes)))
+
+for i in range(len(good_genes)):
+
+    act = hpo_model.perturbation(ontobj=hpo,
+                                 dataset='recount3_GTEx_muscle',
+                                 genes=[good_genes[i]],
+                                 values=[0],
+                                 terms=['HP:0006785'])
+    ko_act[:,i] = act.flatten()
+
+
+# create dataframe
+ko_act = pd.DataFrame(ko_act)
+ko_act.columns = good_genes
 
 # save results
-ko_act.to_csv(project_dir + '/analysis/02_GTEx_HPO/results/systematic_knockout_GTEx_LGMD_activities.csv', index=False)
+ko_act.to_csv(project_dir + '/analysis/03_LGMD/results/systematic_knockout_GTEx_LGMD_activities.csv', index=False)
 
 
